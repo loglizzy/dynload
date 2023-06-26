b7dc7fe6545771e46718b0187c247b0122655ec1
loglizzy <72479668+loglizzy@users.noreply.github.com>
2023-06-25 23:08:11 -0400

    Update main.lua

--- components/main.lua
+++ components/main.lua
@@ -1,15 +1,13 @@
-return function()
-	Iris.Text({"The current time is: " .. os.clock()})
+Iris.Text({"The current time is: " .. os.clock()})
 
-	Iris.InputText({"Enter Text"})
+Iris.InputText({"Enter Text"})
 
-	if Iris.Button({"Click me"}).clicked then
-		print("button was clicked")
-	end
+if Iris.Button({"Click me"}).clicked then
+	print("button was clicked")
+end
 
-	Iris.Table({1})
-	for i = 0, 9 do
-		Iris.Button({"Button - " .. i})
-		Iris.NextColumn()
-	end
+Iris.Table({1})
+for i = 0, 9 do
+	Iris.Button({"Button - " .. i})
+	Iris.NextColumn()
 end
